616c32aaa6e4ae432d1a20c8d616c5fe2fa34244
Paul Chu <lordbabypanda@gmail.com>
2016-05-05 01:28:30 -0500

    Highest Prime Number

--- Prime/Prime.cpp
+++ Prime/Prime.cpp
@@ -5,10 +5,10 @@ bool PrimeNumber(int num)
 	int i;
 	for (i = 2; i < num; ++i)
 	{
-		std::cout << num << " % " << i << " = " << num%i <<std::endl;
+		//std::cout << num << " % " << i << " = " << num%i <<std::endl;
 
 		// Check if num is compare to 0 to see if it's prime or not.
-		if (num % i == 0 || num == 2)
+		if (num % i == 0)
 		{
 			std::cout << "remainder of " << i << ", "<< num << " is not 0" << std::endl;
 			std::cout << "False : Not a Prime Number" << std::endl;
@@ -22,22 +22,32 @@ bool PrimeNumber(int num)
 	
 }
 
-int HighestPrime(int num)
+int HighestPrime(int startNum, int endNum)
 {
-	int numRange[num];
-	return true;
+	int highPrime = 0;
+	for (startNum = 0; startNum <= endNum; startNum++)
+	{
+		PrimeNumber(startNum);
+		std::cout << startNum << std::endl;
+		/*if (startNum > highPrime)
+		{
+			highPrime = startNum;
+			std::cout << "The highest prime number is: " << highPrime << std::endl;
+		}*/
+	}
+	return highPrime;
 }
 
 int main()
 {
 	while (true)
 	{
-		int p;
+		int startNumber, endNumber;
 		std::cout << " " << std::endl;
 		std::cout << "Enter a number: " << std::endl;
-		std::cin >> p;
-		PrimeNumber(p);
-		//HighestPrime(p);
+		std::cin >> startNumber >> endNumber;
+		//PrimeNumber(p);
+		HighestPrime(startNumber, endNumber);
 	}
 	system("pause");
 }
