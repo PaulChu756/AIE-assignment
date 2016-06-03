db23788ac5e2fb7395ec4af0a85898bdbbfdad93
Paul Chu <lordbabypanda@gmail.com>
2016-06-03 14:47:27 -0500

    Comments.

--- Prime/0IntheBack.cpp
+++ Prime/0IntheBack.cpp
@@ -4,18 +4,20 @@ void Zero::ZeroValue(int num[], int length)
 {
 	for (int i = 0; i < length; ++i)
 	{
+		//std::cout << num[i] << ' ';
+
 		if (num[i] == 0)
 		{
 			num[length - 1] = 0; // put the last element of the array to 0
 
-			for (int j = i; j < length; ++j)
+			for (int j = i; j < length; ++j) // start where it found 0
 			{
-				if (j == 9)
+				if (j == 9) // if last element
 				{
-					num[j] = 0;
+					num[j] = 0; // set to 0
 				}
 				else
-					num[j] = num[j + 1];
+					num[j] = num[j + 1]; // current element to grab the next element to push up other elements in the array
 			}
 		}
 		std::cout << num[i] << ' ';
